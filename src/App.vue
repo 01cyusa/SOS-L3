6ba04757b006548e734e0e28be3e841cc9272923
keane2vexxed <aokarenzi13@gmail.com>
2026-05-26 09:46:47 +0200

    my first commit on file project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <title>SOS BLOG</title>
+  <h1 class= "logo logo--top left">
+    <img src="./assets/logo.png" alt="Vue logo" class="logo__image" />
+    
+                              </h1>
+
+
+   <h2 class="text-2xl font-bold text-center text-underline text-italic text-blue-500 mt-8">
+    SOS BLOG
+  </h2>
+
+  <p class="text-4xl text-center text-gray-600 mt-4">
+   SOS Children’s Villages Rwanda is an independent member association affiliated to the SOS Federation. As a leader in childcare, SOS Children’s Villages Rwanda leverages existing community resources to strengthen quality child care and protection through a holistic continuum of care tailored to orphans, vulnerable children and their families. SOS Children’s Villages Rwanda actively advocates for the promotion of childcare and child protection.
+Since 2004, The First Lady of Rwanda, Her Excellency Mrs. Jeannette Kagame is the Patron of SOS Children’s Village Rwanda.
+
+
+  </p>
+</template>
+
+<style scoped></style>
