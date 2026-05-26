504dc104b47c9a279f32798eb87f2cdac8f72266
keane2vexxed <aokarenzi13@gmail.com>
2026-05-26 22:40:41 +0200

    Added Navbr

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
-<script setup></script>
+<script setup>
+import Home from './Components/Home.vue';
+import Navbar from './Components/Navbar.vue';
 
+</script>
 <template>
-  <title>SOS BLOG</title>
-  <h1 class= "logo logo--top left">
-    <img src="./assets/logo.png" alt="Vue logo" class="logo__image" />
-    
-                              </h1>
-
-
-   <h2 class="text-2xl font-bold text-center text-underline text-italic text-blue-500 mt-8">
-    SOS BLOG
-  </h2>
-
-  <p class="text-4xl text-center text-gray-600 mt-4">
-   SOS Children’s Villages Rwanda is an independent member association affiliated to the SOS Federation. As a leader in childcare, SOS Children’s Villages Rwanda leverages existing community resources to strengthen quality child care and protection through a holistic continuum of care tailored to orphans, vulnerable children and their families. SOS Children’s Villages Rwanda actively advocates for the promotion of childcare and child protection.
-Since 2004, The First Lady of Rwanda, Her Excellency Mrs. Jeannette Kagame is the Patron of SOS Children’s Village Rwanda.
-
-
-  </p>
+  <Navbar/>
+  <Home/>
 </template>
-
-<style scoped></style>
