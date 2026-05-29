56a696ca116d6aaab66253be69928bb2e7c8008f
01cyusa <cyusaivan0@gmail.com>
2026-05-29 15:41:39 +0200

    updated project

--- src/Components/Navbar.vue
+++ src/Components/Navbar.vue
@@ -4,12 +4,12 @@
         <span class="text-xl font-bold "></span>
 
         <ul class="flex gap-8 text-sm font-medium w-full justify-end">
-            <li><router-link to="/" class="hover:text-yellow-400 no-underline">HOME</router-link></li>
-            <li><router-link to="/about" class="hover:text-yellow-400 no-underline">ABOUT US</router-link></li>
-            <li><router-link to="/service" class="hover:text-yellow-400 no-underline">SERVICE</router-link></li>
+          <li><router-link to="/" class="hover:text-yellow-400 no-underline">{{ $t('nav.home') }}</router-link></li>
+          <li><router-link to="/about" class="hover:text-yellow-400 no-underline">{{ $t('nav.about') }}</router-link></li>
+          <li><router-link to="/service" class="hover:text-yellow-400 no-underline">{{ $t('nav.service') }}</router-link></li>
              <li class="relative group overflow-visible z-50">
   <button type="button" class="hover:text-yellow-300 inline-flex items-center gap-1 no-underline">
-    GALLERY <span>⌄</span>
+    {{ $t('nav.gallery') }} <span>⌄</span>
   </button>
 
   <ul
@@ -19,20 +19,20 @@
            bg-white py-2 min-w-[10rem] rounded shadow-lg z-50"
   >
     <li>
-      <router-link to="/gallery" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">Gallery Home</router-link>
+      <router-link to="/gallery" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">{{ $t('nav.galleryHome') }}</router-link>
     </li>
     <li>
-      <router-link to="/videos" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">Videos</router-link>
+      <router-link to="/videos" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">{{ $t('nav.videos') }}</router-link>
     </li>
     <li>
-      <router-link to="/gallery/images" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">Images</router-link>
+      <router-link to="/gallery/images" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">{{ $t('nav.images') }}</router-link>
     </li>
   </ul>
 </li>
         
               <li class="relative group overflow-visible z-50">
   <button type="button" class="hover:text-yellow-300 inline-flex items-center gap-1 no-underline">
-    DESTINATION <span>⌄</span>
+    {{ $t('nav.destination') }} <span>⌄</span>
   </button>
 
   <ul
@@ -42,26 +42,61 @@
            bg-white py-2 min-w-[10rem] rounded shadow-lg z-50"
   >
     <li>
-      <router-link to="/destination" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">Destination Home</router-link>
+      <router-link to="/destination" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">{{ $t('nav.destinationHome') }}</router-link>
     </li>
     <li>
-      <router-link to="/province/western" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">Western province</router-link>
+      <router-link to="/province/western" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">{{ $t('nav.western') }}</router-link>
     </li>
     <li>
-      <router-link to="/province/eastern" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">Eastern province</router-link>
+      <router-link to="/province/eastern" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">{{ $t('nav.eastern') }}</router-link>
     </li>
     <li>
-      <router-link to="/province/northern" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">Northern province</router-link>
+      <router-link to="/province/northern" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">{{ $t('nav.northern') }}</router-link>
     </li>
     <li>
-      <router-link to="/province/southern" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">Southern province</router-link>
+      <router-link to="/province/southern" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">{{ $t('nav.southern') }}</router-link>
     </li>
     <li>
-      <router-link to="/province/kigali" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">Kigali City</router-link>
+      <router-link to="/province/kigali" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white no-underline">{{ $t('nav.kigali') }}</router-link>
     </li>
   </ul>
 </li>
-            <li><router-link to="/contact" class="hover:text-yellow-400 no-underline">CONTACT</router-link></li>
+            <li><router-link to="/contact" class="hover:text-yellow-400 no-underline">{{ $t('nav.contact') }}</router-link></li>
+            <li class="relative group overflow-visible z-50">
+  <button type="button" class="hover:text-yellow-300 inline-flex items-center gap-1 no-underline">
+    {{ $t('nav.language') }} <span>⌄</span>
+  </button>
+
+  <ul
+    class="absolute left-0 top-full invisible opacity-0 translate-y-2
+           group-hover:visible group-hover:opacity-100 group-hover:translate-y-0
+           transition-all duration-300
+           bg-white py-2 min-w-[8rem] rounded shadow-lg z-50"
+  >
+    <li>
+      <button @click="changeLang('en')" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white w-full text-left">{{ $t('nav.english') }}</button>
+    </li>
+    <li>
+      <button @click="changeLang('fr')" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white w-full text-left">{{ $t('nav.french') }}</button>
+    </li>
+    <li>
+      <button @click="changeLang('rw')" class="block px-4 py-2 text-black hover:bg-green-700 hover:text-white w-full text-left">{{ $t('nav.kinyarwanda') }}</button>
+    </li>
+  </ul>
+</li>
         </ul>
         </nav>
     </template>
+
+<script>
+export default {
+  name: 'Navbar',
+  methods: {
+    changeLang(lang) {
+      if (this.$i18n && this.$i18n.locale) {
+        this.$i18n.locale = lang
+      }
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,23 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { createI18n } from 'vue-i18n'
+import en from './locales/en.json'
+import fr from './locales/fr.json'
+import rw from './locales/rw.json'
+
+const messages = { en, fr, rw }
+
+const i18n = createI18n({
+	legacy: true,
+	locale: 'en',
+	fallbackLocale: 'en',
+	messages
+})
 
 const app = createApp(App)
 
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
